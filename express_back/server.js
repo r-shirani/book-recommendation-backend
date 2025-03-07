48b68684d7b418d525464122e9f474a9b4a0ba8a
AhmadEmrani <mrany393@gmail.com>
2025-03-07 16:41:55 +0330

    routes and server.js added

---
+++ express_back/server.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
