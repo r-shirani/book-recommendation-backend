91be2c975500cf5e4a1341b093f5018d8ef698be
AhmadEmrani <mrany393@gmail.com>
2025-03-07 16:38:51 +0330

    adding middlewares and controllers

---
+++ express_back/controllers/authcontroller.js
@@ -0,0 +1,39 @@
+const User = require("/models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+exports.register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    let user = await User.findOne({ email });
+
+    if (user) return res.status(400).json({ message: "کاربر قبلاً ثبت شده است" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    user = new User({ name, email, password: hashedPassword });
+    await user.save();
+
+    res.status(201).json({ message: "ثبت‌نام با موفقیت انجام شد" });
+  } catch (error) {
+    res.status(500).json({ message: "خطای سرور" });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) return res.status(400).json({ message: "کاربر یافت نشد" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: "رمز عبور نادرست است" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+    res.json({ token, user: { id: user._id, name: user.name, email: user.email } });
+  } catch (error) {
+    res.status(500).json({ message: "خطای سرور" });
+  }
+};
