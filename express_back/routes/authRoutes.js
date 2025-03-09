19389f93b4eb1815557ad94bc2e11ce2ca89ed09
r-shirani <rsh96785@gmail.com>
2025-03-09 21:26:13 +0330

    add validation for login and refister route

--- express_back/routes/authRoutes.js
+++ express_back/routes/authRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { register, login, getProfile } = require("../controllers/authcontroller");
 const authMiddleware = require("../middlewares/authMiddleware");
+const { validateRegister, validateLogin } = require("../validators/authValidator");
 
 const router = express.Router();
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register",validateRegister ,register);
+router.post("/login",validateLogin ,login);
 router.get("/profile", authMiddleware, getProfile);  // protected with token ()
 
 module.exports = router;
