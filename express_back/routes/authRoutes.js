e73784d59175b504fb3418cd7e697ccfbe4b10a2
r-shirani <rsh96785@gmail.com>
2025-03-09 21:09:41 +0330

    fix some stupid mistake in authRoutes.js file

--- express_back/routes/authRoutes.js
+++ express_back/routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { register, login, getProfile } = require("../controllers/authController");
+const { register, login, getProfile } = require("../controllers/authcontroller");
 const authMiddleware = require("../middlewares/authMiddleware");
 
 const router = express.Router();
