fb66210f3eacdc2fd2d7fb3cff14d4a6eeb55a26
Amirali <amiralighadiri1383@gmail.com>
2025-03-09 12:57:18 +0330

    Table of user type

---
+++ DBScripts/User.UserType.sql
@@ -0,0 +1,22 @@
+USE [BookWormDB]
+GO
+
+/****** Object:  Table [User].[UserType]    Script Date: 19/12/1403 12:58:05 ق.ظ ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [User].[UserType](
+	[UserTypeID] [tinyint] NOT NULL,
+	[Code] [nvarchar](10) NOT NULL,
+	[MainName] [nvarchar](50) NULL,
+	[Description] [nvarchar](250) NULL,
+ CONSTRAINT [PK_UserType] PRIMARY KEY CLUSTERED 
+(
+	[UserTypeID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
