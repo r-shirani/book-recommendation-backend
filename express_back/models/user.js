6ce26aa222b2aa75fbb84ff020673ee0c67ba605
Ahmad emrani <127972023+AhmadEmrani@users.noreply.github.com>
2025-03-11 05:40:03 -0700

    fixing validation

--- express_back/models/user.js
+++ express_back/models/user.js
@@ -1,9 +1,11 @@
+
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
+    googleId: { type: String, unique: true },
+    email: { type: String, required: true, unique: true },
+    name: { type: String },
+    avatar: { type: String },
+}, { timestamps: true });
 
 module.exports = mongoose.model("User", UserSchema);
