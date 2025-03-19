f3c0695c21088b10344b3647f9b6189b4be0e509
AmirAli_2 <amiralighadiri1383@gmail.com>
2025-03-19 18:03:42 +0330

    Edit signup API for taking verifyCode

--- DB_APIs/Server/src/controller/Controller.User.pas
+++ DB_APIs/Server/src/controller/Controller.User.pas
@@ -50,7 +50,8 @@ Type
           Const [MVCFromQueryString('name', '')] Name: String;
           Const [MVCFromQueryString('password', '')] Password: String;
           Const [MVCFromQueryString('email', '')] email: String;
-          Const [MVCFromQueryString('clientEmailID', '')] CEmailID: String
+          Const [MVCFromQueryString('clientEmailID', '')] CEmailID: String;
+          Const [MVCFromQueryString('VerificationCode', '')] VerifyCode: String
         );
 End;
 
@@ -89,7 +90,8 @@ Begin
     Render(ObjectDict().Add('data', TMVCActiveRecord.GetByPK<Model.User.TUser>(userID)));
 End;
 //______________________________________________________________________________
-Procedure TUserController.SignupNormalUser(Const Name, Password, Email, CEmailID: String);
+Procedure TUserController.SignupNormalUser(Const Name, Password, Email, CEmailID,
+  VerifyCode: String);
 Var
     FDStoredProc: TFDStoredProc;
 Begin
@@ -108,11 +110,17 @@ Begin
         FDStoredProc.ParamByName('@name').AsString := Name;
         FDStoredProc.ParamByName('@pass').AsString := Password;
         FDStoredProc.ParamByName('@email').AsString := Email;
-            
-        If CEmailID.IsEmpty then 
+
+        If CEmailID.IsEmpty then
             FDStoredProc.ParamByName('@clientEmailID').Value := Null
-        Else 
+        Else
             FDStoredProc.ParamByName('@clientEmailID').AsString := CEmailID;
+
+
+        If VerifyCode.IsEmpty then
+            FDStoredProc.ParamByName('@VerificationCode').Value := Null
+        Else
+            FDStoredProc.ParamByName('@VerificationCode').AsString := VerifyCode;
 
         Try           
             FDStoredProc.Open;
---
+++ DB_APIs/Server/src/dataModule/__history/UDMMain.dfm.~9~
@@ -0,0 +1,9 @@
+object DMMain: TDMMain
+  Height = 186
+  Width = 291
+  object FDConn: TFDConnection
+    LoginPrompt = False
+    Left = 59
+    Top = 27
+  end
+end
