0bcad1dcadd0c999d40b0c4eabd420bf12215774
r-shirani <rsh96785@gmail.com>
2025-03-23 23:36:12 +0330

    implement authentication middleware

--- express_back/middlewares/authMiddleware.js
+++ express_back/middlewares/authMiddleware.js
@@ -1,17 +1,20 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-  const token = req.header("Authorization");
-  if (!token) return res.status(401).json({ message: "Authentication failed. Please login again." });
-  maintoken = token.split(" ")[1];
+  const authHeader = req.header("Authorization");
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Authentication failed. Please login again." });
+  }
+
+  const token = authHeader.split(" ")[1];
   try {
-    const decoded = jwt.verify(maintoken, process.env.JWT_SECRET);
-    req.user = decoded;
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded; //store user info in req.user for use in controllers
     next();
   } catch (err) {
     if (err.name === "TokenExpiredError") {
-      return res.status(401).json({ message: "token expired! please login again" });
+      return res.status(401).json({ message: "Token expired! Please login again." });
     }
-    res.status(401).json({ message: "invalid token" });
+    res.status(401).json({ message: "Invalid token" });
   }
 };
