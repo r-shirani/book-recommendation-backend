3274601ad6ba1b6e06cf7513b088fe55350838ed
AhmadEmrani <mrany393@gmail.com>
2025-04-04 16:52:45 +0330

    updating server.js file

--- express_back/server.js
+++ express_back/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 //const connectDB = require("./configs/db");
 const authRoutes = require("./routes/authRoutes");
+const commentRoutes = require("./routes/commentRoutes")
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -16,6 +17,7 @@ app.use(cors());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api",commentRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
