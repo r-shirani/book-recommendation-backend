4032518c4808272cd39bdb18b97085396bd5fcf9
AhmadEmrani <mrany393@gmail.com>
2025-04-07 01:01:13 +0330

    adding get image and search controllers

--- express_back/SQL/SQL-book-controller.js
+++ express_back/SQL/SQL-book-controller.js
@@ -1,5 +1,5 @@
 const bcrypt = require("bcryptjs");
-
+const axios = require('axios');
 const book = axios.create({
     baseURL: "http://185.255.90.36:9547/api/v1/book", 
     headers: {
@@ -28,8 +28,27 @@ const searchBook_controller = async (searchterm_input , pageNum_input ,count_inp
     }
 }
 
+const bookImage = async (bookid)=>{
+    const baseurl = "http://185.255.90.36:9547/api/v1/images";
+    const image_info = await axios.get(baseurl+`/book/${bookid}`);
+    const{list} = image_info.data;
+    const imageguid =list[0].imageguid;
+    const image_file_url = baseurl + `/file/${imageguid}`;
 
+    try {
+        const response = await axios.get(image_file_url, { responseType: 'stream' });
+        return {
+          stream: response.data,
+          contentType: response.headers['content-type'],
+        };
+    } 
+    catch (error) {
+        throw new Error('Failed to fetch image');
+    }
+   
+}
 
 module.exports={
-    searchBook_controller
+    searchBook_controller,
+    bookImage
 }
