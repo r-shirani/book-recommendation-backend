a118660a02f1438db32b162c4287b4cf7f0a8300
AhmadEmrani <mrany393@gmail.com>
2025-04-07 01:02:05 +0330

    implementing controllers for search and image

--- express_back/controllers/bookController.js
+++ express_back/controllers/bookController.js
@@ -3,9 +3,35 @@ const { validationResult } = require("express-validator");
 const jwt = require("jsonwebtoken");
 const sendVerificationCode = require('../Auth/mailer');
 const { getUserByID } = require("../SQL/SQL-user-controller");
+const { bookImage, searchBook_controller } = require("../SQL/SQL-book-controller");
 
 
 
-exports.searchBook = async ()=>{
-    
+exports.searchBook = async (req , res)=>{
+    const {pagenum , searchterm} = req.body;
+    const count =10;
+    const bookData = await searchBook_controller(searchterm,pagenum,count);
+    if(bookData != -1){
+        res.status(200).json({
+            bookData
+        })
+    }
+    else{
+        res.status(500).json({ message: "server error" });
+    }
+
+}
+
+
+exports.getBookImage = async (req , res) =>{
+    const bookid = req.params.bookid;
+    try {
+        const { stream, contentType } = await bookImage(bookid);
+        res.setHeader('Content-Type', contentType);
+        stream.pipe(res);
+        console.log(`Image sent successfully bookid : ${bookid}`); // Stream the image to the response
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json( {message : "server error - (error in fetching image)" });
+    } 
 }
