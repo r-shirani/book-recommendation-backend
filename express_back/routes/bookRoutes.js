f022ad74cccccc97b773fae65859a2e4fd917d9f
r-shirani <rsh96785@gmail.com>
2025-04-09 00:11:43 +0330

    add a route for get popular books

--- express_back/routes/bookRoutes.js
+++ express_back/routes/bookRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const authMiddleware = require("../middlewares/authMiddleware");
-const { searchBook, getBookImage } = require("../controllers/bookController");
+const { searchBook, getBookImage , popularBooks } = require("../controllers/bookController");
 
 const router = express.Router();
 
 router.get("/search", searchBook );
 router.get("/image/:bookid",getBookImage);
+router.get("/popularBooks",popularBooks);
 
 module.exports = router;
