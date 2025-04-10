39a8adf8513511ba1733f09ad3507a775dfcaa96
AhmadEmrani <mrany393@gmail.com>
2025-04-10 03:30:25 +0330

    updating controller in search routes

--- express_back/routes/bookRoutes.js
+++ express_back/routes/bookRoutes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const authMiddleware = require("../middlewares/authMiddleware");
-const { searchBook, getBookImage , popularBooks } = require("../controllers/bookController");
+const { searchBook, getBookImage , popularBooks, searchBook_with_image } = require("../controllers/bookController");
 
 const router = express.Router();
 
-router.get("/search", searchBook );
+router.get("/search", searchBook_with_image );
 router.get("/image/:bookid",getBookImage);
 router.get("/popularBooks",popularBooks);
 
