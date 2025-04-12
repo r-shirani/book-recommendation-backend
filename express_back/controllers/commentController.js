5c118692c0dd3a85a101c82f4c49047e4be2f5fa
R_shirani <127476888+r-shirani@users.noreply.github.com>
2025-04-13 01:57:37 +0330

    Update commentController.js

--- express_back/controllers/commentController.js
+++ express_back/controllers/commentController.js
@@ -5,21 +5,17 @@ const sendVerificationCode = require('../Auth/mailer');
 const { getUserByID } = require("../SQL/SQL-user-controller");
 const { postComment_controller } = require("../SQL/SQL-comment-controller");
 
-
 exports.newComment = async (req , res)=>{
     try {
         const userID = req.user.id; //"Data retrieved from middleware"
         const {bookid , text} = req.body;
-
         const response = await postComment_controller(userID , bookid , text);
         if(response===-1){
             res.status(500).json({ message: "server error(SQL)" });
         }
-        else if(response===1)
-        {
+        else if(response===1){
             res.status(201).json({ message: "Comment added successfully" });
         }
-
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: "server error" });
