a693b8d49619ede7e6c1383e63c92149c14c5125
AhmadEmrani <mrany393@gmail.com>
2025-04-17 03:39:08 +0330

    adding routes for genres

--- express_back/routes/authRoutes.js
+++ express_back/routes/authRoutes.js
@@ -2,15 +2,20 @@ const express = require("express");
 const { register, login, getProfile ,verifyCode, googleLogin, newPassword,updateProfile} = require("../controllers/authcontroller");
 const authMiddleware = require("../middlewares/authMiddleware");
 const { validateRegister, validateLogin } = require("../validators/authValidator");
+const { getUserGenres, updateUserGenres } = require("../controllers/genresController");
+
 
 const router = express.Router();
 
 router.post("/register",validateRegister ,register);
 router.post("/login",validateLogin ,login);
 router.get("/profile", authMiddleware, getProfile);  // protected with token ()
-router.put("/newPassword",authMiddleware, newPassword)
+router.put("/newPassword",authMiddleware, newPassword);
 router.post('/verify-code', verifyCode);
 router.post("/google-login",googleLogin);
-router.put("/updateProfile",authMiddleware,updateProfile)
+router.put("/updateProfile",authMiddleware,updateProfile);
+router.get("/genres",authMiddleware,getUserGenres);
+router.put("/genres",authMiddleware,updateUserGenres);
+
 
 module.exports = router;
