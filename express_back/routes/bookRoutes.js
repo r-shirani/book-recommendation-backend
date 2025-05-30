3752d83ebb2861b332320095ef91e484c65be5ae
r-shirani <rsh96785@gmail.com>
2025-05-31 02:10:21 +0330

    router of getting books by MBTI of user

--- express_back/routes/bookRoutes.js
+++ express_back/routes/bookRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const authMiddleware = require("../middlewares/authMiddleware");
-const { searchBook, getBookImage , popularBooks, searchBook_with_image, like_book, dislike_book, favoritBooks , getBookDetail, likeStatusController} = require("../controllers/bookController");
+const { searchBook, getBookImage , popularBooks, searchBook_with_image, like_book, dislike_book, favoritBooks , getBookDetail, likeStatusController,MBTI_Books} = require("../controllers/bookController");
 
 const router = express.Router();
+console.log("MBTI_Books typeof:", typeof MBTI_Books);
+
 
 router.get("/search", searchBook_with_image );
 router.get("/searchurl/:searchterm",searchBook)
@@ -13,5 +15,6 @@ router.delete("/dislike" ,authMiddleware , dislike_book );
 router.get("/favorit" , authMiddleware , favoritBooks);
 router.get('/bookdetail', getBookDetail);
 router.get('/likestatus', authMiddleware ,likeStatusController);
+router.get('/MBTIbooks',authMiddleware, MBTI_Books);
 
 module.exports = router;
